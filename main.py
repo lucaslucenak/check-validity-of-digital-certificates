53a7b9680732ee4b534c41019f6f23290f62264d
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-05 21:21:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import ssl
 import datetime
-# import mysql.connector
 
 from datetime import date
 from datetime import datetime
@@ -9,83 +8,12 @@ import xlwt
 from tkinter import *
 from tkinter import messagebox
 from cryptography import x509
-# @Cnum#
 from entities.Certificado import Certificado
 
 maxDays = 10
 
-# Way to save it into a SQL table (import mysql.connector)
-# ########################################################################################################################
-# # -----------------------------------------Operações e conexão do Banco de Dados---------------------------------------#
-# ########################################################################################################################
-#
-# db = mysql.connector.connect(
-#     host='localhost',
-#     user='root',
-#     password='',
-#     database='info_certificados'
-# )
-# cursor = db.cursor()
-#
-# cursor.execute('drop table tb_certificados')
-# sqlCreateTable = "CREATE TABLE tb_certificados (id INT unsigned auto_increment primary key not null," \
-#                  "emitido_por varchar(255)," \
-#                  "valido_a_partir varchar(255)," \
-#                  "valido_ate varchar(255)," \
-#                  "tempo_restante varchar(255)," \
-#                  "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP," \
-#                  "updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)"
-# cursor.execute(sqlCreateTable)
-#
-# cursor.execute('drop table tb_certificadosWarning')
-# sqlCreateTable = "CREATE TABLE tb_certificadosWarning (id INT unsigned auto_increment primary key not null," \
-#                  "emitido_por varchar(255)," \
-#                  "valido_a_partir varchar(255)," \
-#                  "valido_ate varchar(255)," \
-#                  "tempo_restante varchar(255)," \
-#                  "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP," \
-#                  "updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)"
-# cursor.execute(sqlCreateTable)
-#
-#
-# def create(emitidoPor, validoApartirDe, validoAte, tempoRestante, table):
-#     sqlInsert = f"INSERT INTO {table} (emitido_por, valido_a_partir, valido_ate, tempo_restante) VALUES (%s, %s, %s, %s)"
-#     data = (rf"{emitidoPor}", rf"{validoApartirDe}", rf"{validoAte}", rf"{tempoRestante}")
-#     cursor.execute(sqlInsert, data)
-#     db.commit()
-#
-#
-# def readAll(table):
-#     cursor = db.cursor()
-#     sqlSelect = f"SELECT * FROM {table}"
-#     cursor.execute(sqlSelect)
-#     return cursor.fetchall()
-#
-#
-# def read_emitidoPor(emitidoPor, table):
-#     sqlSelect = f"SELECT * FROM {table} where emitido_por = '{emitidoPor}'"
-#     cursor.execute(sqlSelect)
-#     return cursor.fetchall()
-#
-#
-# def delete(emitidoPor, table):
-#     sqlDelete = f"DELETE FROM {table} WHERE emitido_por = '{emitidoPor}'"
-#     cursor.execute(sqlDelete)
-#     db.commit()
-#
-#     if len(read_emitidoPor(emitidoPor, 'tb_certificados')) == 0:
-#         print('Deletado com sucesso.')
-#     else:
-#         print('Houve um problema.')
-#
-#
-# def update_emitidoPor(actualName, newName, table):
-#     sqlUpdate = f"UPDATE {table} SET emitido_por = '{newName}' WHERE emitido_por = '{actualName}'"
-#     cursor.execute(sqlUpdate)
-#     db.commit()
-
 ########################################################################################################################
-# -------------------------------------------------------Funções-------------------------------------------------------#
+# -------------------------------------------------------Functions-----------------------------------------------------#
 ########################################################################################################################
 
 
@@ -143,53 +71,45 @@ for i in range(len(certificadosList)):
     if int(certificadosList[i].getTempoRestante()) <= maxDays:
         certificadosWarning.append(certificadosList[i])
 
-# # Atualiza as informações no banco de dados
-# for i in range(len(certificadosList)):
-#     create(certificadosList[i].getEmitidoPor(), certificadosList[i].getValidoApartirDe(),
-#            certificadosList[i].getValidoAte(), certificadosList[i].getTempoRestante(), 'tb_certificados')
-#
-# for i in range(len(certificadosWarning)):
-#     create(certificadosWarning[i].getEmitidoPor(), certificadosWarning[i].getValidoApartirDe(),
-#            certificadosWarning[i].getValidoAte(), certificadosWarning[i].getTempoRestante(), 'tb_certificadosWarning')
-
-
 ########################################################################################################################
 # -------------------------------------------------Salva dados em um .xls----------------------------------------------#
 ########################################################################################################################
-
 # Arquivo com todos os certificados
-workbook = xlwt.Workbook()
-resultSheet = workbook.add_sheet('ResultSheet')
 
-# Escreve o header
-header = ['Emitido por', 'Valido a partir de', 'Válido ate', 'Tempo restante (Dias)']
-for h in range(len(header)):
-    resultSheet.write(0, h, header[h])
+if len(certificadosList) > 0:
+    workbook = xlwt.Workbook()
+    resultSheet = workbook.add_sheet('ResultSheet')
 
-for i in range(len(certificadosList)):
-    resultSheet.write(i + 1, 0, certificadosList[i].getEmitidoPor())
-    resultSheet.write(i + 1, 1, certificadosList[i].getValidoApartirDe().strftime("%d/%m/%Y"))
-    resultSheet.write(i + 1, 2, certificadosList[i].getValidoAte().strftime("%d/%m/%Y"))
-    resultSheet.write(i + 1, 3, certificadosList[i].getTempoRestante())
+    # Escreve o header
+    header = ['Emitido por', 'Valido a partir de', 'Válido ate', 'Tempo restante (Dias)']
+    for h in range(len(header)):
+        resultSheet.write(0, h, header[h])
 
-workbook.save(rf'resumo.xls')
+    for i in range(len(certificadosList)):
+        resultSheet.write(i + 1, 0, certificadosList[i].getEmitidoPor())
+        resultSheet.write(i + 1, 1, certificadosList[i].getValidoApartirDe().strftime("%d/%m/%Y"))
+        resultSheet.write(i + 1, 2, certificadosList[i].getValidoAte().strftime("%d/%m/%Y"))
+        resultSheet.write(i + 1, 3, certificadosList[i].getTempoRestante())
 
-# Arquivo com os certificados próximo do vencimento
-workbook = xlwt.Workbook()
-resultSheet = workbook.add_sheet('ResultSheet')
+    workbook.save(rf'resum.xls')
 
-# Escreve o header
-header = ['Emitido por', 'Valido a partir de', 'Válido ate', 'Tempo restante (Dias)']
-for h in range(len(header)):
-    resultSheet.write(0, h, header[h])
+    # Arquivo com os certificados próximo do vencimento
+    if len(certificadosWarning) > 0:
+        workbook = xlwt.Workbook()
+        resultSheet = workbook.add_sheet('ResultSheet')
 
-for i in range(len(certificadosWarning)):
-    resultSheet.write(i + 1, 0, certificadosWarning[i].getEmitidoPor())
-    resultSheet.write(i + 1, 1, certificadosWarning[i].getValidoApartirDe().strftime("%d/%m/%Y"))
-    resultSheet.write(i + 1, 2, certificadosWarning[i].getValidoAte().strftime("%d/%m/%Y"))
-    resultSheet.write(i + 1, 3, certificadosWarning[i].getTempoRestante())
+        # Escreve o header
+        header = ['Emitido por', 'Valido a partir de', 'Válido ate', 'Tempo restante (Dias)']
+        for h in range(len(header)):
+            resultSheet.write(0, h, header[h])
 
-workbook.save(rf'resumo_proxVencimento.xls')
+        for i in range(len(certificadosWarning)):
+            resultSheet.write(i + 1, 0, certificadosWarning[i].getEmitidoPor())
+            resultSheet.write(i + 1, 1, certificadosWarning[i].getValidoApartirDe().strftime("%d/%m/%Y"))
+            resultSheet.write(i + 1, 2, certificadosWarning[i].getValidoAte().strftime("%d/%m/%Y"))
+            resultSheet.write(i + 1, 3, certificadosWarning[i].getTempoRestante())
+
+        workbook.save(rf'near_from_due_date.xls')
 ########################################################################################################################
 # ----------------------------------------------------Janela de aviso--------------------------------------------------#
 ########################################################################################################################
